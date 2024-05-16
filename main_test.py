01aef8c33397b070c7cdd68969a9d1a5b952b7ff
sitao <sitaomin@usc.edu>
2024-05-15 23:27:36 -0400

    new

--- main_test.py
+++ main_test.py
@@ -2,19 +2,19 @@ import os
 import json
 import numpy as np
 import hydra
+import torch
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base=None, config_path="config", config_name="experiment_config")
-def main(seed=10):
-    rng = np.random.default_rng(seed=seed)
-
-    a = 0
-    while a <= 10:
-        ret = rng.dirichlet([0.1, 0.1, 0.1])
-        a = a + 1
-        print(ret)
+@hydra.main(version_base=None, config_path="config", config_name="test_config")
+def main(cfg: DictConfig) -> None:
+    config_dict = OmegaConf.to_container(cfg, resolve=True)
+    print("job id: {} gpu available: {} gpu: {} seed: {}".format(
+        HydraConfig.get().job.num, torch.cuda.is_available(), torch.cuda.get_device_name(), config_dict['seed']
+    ))
 
 
 if __name__ == '__main__':
-    main(10)
+    main()
 
