74cce3e1732e877c5c8e1357a665457b852aca4e
sitaomin1994 <sm2370@amarel1.amarel.rutgers.edu>
2024-05-19 00:35:44 -0400

    script

--- main_test.py
+++ main_test.py
@@ -14,7 +14,16 @@ def main(cfg: DictConfig) -> None:
         HydraConfig.get().job.num, torch.cuda.is_available(), torch.cuda.get_device_name(), config_dict['seed']
     ))
 
+def setup_signal_handlers():     
+    import signal
+
+    signal.signal(signal.SIGUSR1, signal.SIG_IGN)  # ignore SIGUSR1
+    signal.signal(signal.SIGUSR2, signal.SIG_IGN)  # ignore SIGUSR2
+    signal.signal(signal.SIGCONT, signal.SIG_IGN)  # ignore SIGCONT
+    signal.signal(signal.SIGTERM, signal.SIG_IGN)  # ignore SIGTERM
+    signal.signal(signal.SIGHUP, signal.SIG_IGN)  # ignore SIGTSTP
 
 if __name__ == '__main__':
+    setup_signal_handlers()
     main()
 
