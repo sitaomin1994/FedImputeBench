96a7a72dc1ce0d7d6de64d75d29e6ee999ebf3d2
sitao <sitaomin@usc.edu>
2024-05-19 19:01:33 -0400

    bug fix gain, memory debug

--- scripts/run_scenario_gpu_test.sh
+++ scripts/run_scenario_gpu_test.sh
@@ -8,12 +8,12 @@
 #round_id=0,1,2,3,4,5,6,7,8,9
 
 export CUBLAS_WORKSPACE_CONFIG=":4096:8"
-python run_fed_imp_scenario.py --multirun \
+python run_fed_imp_scenario.py \
 dataset_name=codrna \
-imputer=gain \
-data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2,niid-f1,niid-f2 \
-missing_scenario_name=mcar,mar-homog,mar-heter,mnar1-homog,mnar1-heter,mnar2-homog,mnar2-heter \
-fed_strategy=local,central,fedavg,fedavg_ft,fedprox \
-round_id=0,1,2,3,4,5,6,7,8,9
+imputer=miwae \
+data_partition_name=iid-even \
+missing_scenario_name=mcar \
+fed_strategy=local \
+round_id=0
 
 
