dc4b5c2ec845ea01294a0c437dd09afa2bb53d50
sitao <sitaomin@usc.edu>
2024-05-22 17:43:26 -0400

    bug fix gain, memory debug

--- scripts/run_scenario_cpu2.sh
+++ scripts/run_scenario_cpu2.sh
@@ -6,10 +6,10 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 command="python run_fed_imp_scenario.py --multirun \
 hydra.launcher.n_jobs=40 \
 dataset_name=hhip \
-imputer=missforest \
+imputer=simple,em,linear_ice \
 data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2,niid-f1,niid-f2 \
 missing_scenario_name=$scenario \
-fed_strategy=fedtree \
+fed_strategy=fedavg \
 round_id=0,1,2,3,4,5,6,7,8,9"
 
 echo $command
