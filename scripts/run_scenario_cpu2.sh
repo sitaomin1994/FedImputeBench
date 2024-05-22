0f4fd290b2e15ccead5c81038ba4abafb61347ad
sitao <sitaomin@usc.edu>
2024-05-22 08:55:27 -0400

    bug fix gain, memory debug

--- scripts/run_scenario_cpu2.sh
+++ scripts/run_scenario_cpu2.sh
@@ -1,30 +1,16 @@
-#export CUBLAS_WORKSPACE_CONFIG=":4096:8"
-#python run_fed_imp_scenario.py --multirun \
-#hydra.launcher.n_jobs=-1 \
-#dataset_name=california \
-#imputer=simple \
-#data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2,niid-f1,niid-f2 \
-#missing_scenario_name=mcar,mar-homog,mar-heter,mnar1-homog,mnar1-heter,mnar2-homog,mnar2-heter \
-#fed_strategy=local,fedavg,central \
-#round_id=0,1,2,3,4,5,6,7,8,9
-#
-#export CUBLAS_WORKSPACE_CONFIG=":4096:8"
-#python run_fed_imp_scenario.py --multirun \
-#hydra.launcher.n_jobs=-1 \
-#dataset_name=california \
-#imputer=em,linear_ice \
-#data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2,niid-f1,niid-f2 \
-#missing_scenario_name=mcar,mar-homog,mar-heter,mnar1-homog,mnar1-heter,mnar2-homog,mnar2-heter \
-#fed_strategy=local,fedavg,central \
-#round_id=0,1,2,3,4,5,6,7,8,9
+#!/bin/bash
+
+scenario=$1
 
 export CUBLAS_WORKSPACE_CONFIG=":4096:8"
-python run_fed_imp_scenario.py --multirun \
-hydra.launcher.n_jobs=-1 \
+command="python run_fed_imp_scenario.py --multirun \
+hydra.launcher.n_jobs=40 \
 dataset_name=hhip \
 imputer=missforest \
 data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2,niid-f1,niid-f2 \
-missing_scenario_name=mcar,mar-homog,mar-heter,mnar1-homog,mnar1-heter,mnar2-homog,mnar2-heter \
+missing_scenario_name=$scenario \
 fed_strategy=fedtree \
-round_id=0,1,2,3,4,5,6,7,8,9
+round_id=0,1,2,3,4,5,6,7,8,9"
 
+echo $command
+eval $command
