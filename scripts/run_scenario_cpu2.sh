bb62e2189d89bde7559aff40d43f478c5b1829cd
sitao <sitaomin@usc.edu>
2024-05-22 20:20:15 -0400

    bug fix gain, memory debug

--- scripts/run_scenario_cpu2.sh
+++ scripts/run_scenario_cpu2.sh
@@ -7,9 +7,9 @@ command="python run_fed_imp_scenario.py --multirun \
 hydra.launcher.n_jobs=40 \
 dataset_name=hhip \
 imputer=missforest \
-data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2,niid-f1,niid-f2 \
+data_partition_name=niid-f2 \
 missing_scenario_name=$scenario \
-fed_strategy=fedtree \
+fed_strategy=local,central \
 round_id=0,1,2,3,4,5,6,7,8,9"
 
 echo $command
