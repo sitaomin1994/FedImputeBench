0cc8410fef499a22141d862ddc6b9a69bac1959d
sitao <sitaomin@usc.edu>
2024-05-26 20:02:01 -0400

    bug fix gain, memory debug

--- scripts/run_scenario_cpu2.sh
+++ scripts/run_scenario_cpu2.sh
@@ -4,12 +4,25 @@ scenario=$1
 
 export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 command="python run_fed_imp_scenario.py --multirun \
-hydra.launcher.n_jobs=40 \
-dataset_name=codrna,california \
-imputer=em \
-data_partition_name=iid-even,iid-uneven,niid-t1,niid-t2 \
-missing_scenario_name=$scenario \
-fed_strategy=local,central \
+hydra.launcher.n_jobs=24 \
+dataset_name=vehicle \
+imputer=missforest \
+data_partition_name=iid-even,iid-uneven,niid-f1,niid-f2 \
+missing_scenario_name=mnar2-homog \
+fed_strategy=local,fedtree,central \
+round_id=0,1,2,3,4,5,6,7,8,9"
+
+echo $command
+eval $command
+
+export CUBLAS_WORKSPACE_CONFIG=":4096:8"
+command="python run_fed_imp_scenario.py --multirun \
+hydra.launcher.n_jobs=24 \
+dataset_name=hhip \
+imputer=missforest \
+data_partition_name=iid-uneven \
+missing_scenario_name=mnar1-homog \
+fed_strategy=local,fedtree,central \
 round_id=0,1,2,3,4,5,6,7,8,9"
 
 echo $command
