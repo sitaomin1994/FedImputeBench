decb1230314b098a47987bd2ec28e1ca99b6c792
sitao <sitaomin@usc.edu>
2024-05-29 15:33:24 -0400

    downstream evaluation

--- scripts/gpu1.sh
+++ scripts/gpu1.sh
@@ -1,2 +1,2 @@
-./scripts/run_scenario_gpu2.sh school_pca miwae iid-even central -1
-./scripts/run_scenario_gpu.sh school_pca gain mcar central -1
+#!/bin/bash
+./scripts/run_scenario_gpu.sh vehicle miwae mnar1-homog central -1
