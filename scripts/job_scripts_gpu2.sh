80b223aaf9960cc4fdb66b53ed75926b09a0591f
sitao <sitaomin@usc.edu>
2024-05-29 17:06:11 -0400

    downstream evaluation

--- scripts/job_scripts_gpu2.sh
+++ scripts/job_scripts_gpu2.sh
@@ -6,8 +6,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=6
+#SBATCH --mem-per-cpu=10G
+#SBATCH --nodelist=gpuk001,gpuk002
 #SBATCH --time=3-00:00:00
 #SBATCH --export=ALL
 
