93cdebf3c8cd8e09a7c361745d67133c2c820854
sitao <sitaomin@usc.edu>
2024-06-01 13:15:32 -0400

    bug fix: config persist - fine-tune overwritten. new feature: add use default hyper params option

--- scripts/gpu2.sh
+++ scripts/gpu2.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 # fedavg
 export CUBLAS_WORKSPACE_CONFIG=":4096:8"
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=iid-even missing_scenario_name=mcar fed_strategy=central round_id=0
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=iid-even missing_scenario_name=mcar fed_strategy=central round_id=4
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=iid-even missing_scenario_name=mar-homog fed_strategy=central round_id=0
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=iid-even missing_scenario_name=mar-homog fed_strategy=central round_id=2
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t1 missing_scenario_name=mnar2-heter fed_strategy=central round_id=3
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t1 missing_scenario_name=mnar2-heter fed_strategy=central round_id=4
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t2 missing_scenario_name=mcar fed_strategy=central round_id=0
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t2 missing_scenario_name=mcar fed_strategy=central round_id=4
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t2 missing_scenario_name=mar-homog fed_strategy=central round_id=0
-python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t2 missing_scenario_name=mar-homog fed_strategy=central round_id=1
+python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t1 missing_scenario_name=mcar fed_strategy=fedavg_ft round_id=0
+python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t1 missing_scenario_name=mcar fed_strategy=fedavg_ft round_id=1
+python run_fed_imp_scenario.py dataset_name=hhip imputer=miwae data_partition_name=niid-t1 missing_scenario_name=mcar fed_strategy=fedavg_ft round_id=3
