d785e088af195639e623bd9c0c2b7260a758f5d9
sitao <sitaomin@usc.edu>
2024-06-02 01:18:54 -0400

    bug fix: config persist - fine-tune overwritten. new feature: add use default hyper params option

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -9,3 +9,7 @@ fed_strategy_name=local,fedavg,fedavg_ft,fedprox,central round_idx=0,1,4,2,3 eva
 python run_evaluation.py --multirun hydra.launcher.n_jobs=-1 dataset_name=hhip \
 data_partition_name=niid-t1,niid-t2,iid-even missing_scenario_name=mcar,mar-heter,mnar2-heter,mnar2-homog,mar-homog imputer_name=gain \
 fed_strategy_name=local,fedavg,fedavg_ft,fedprox,central round_idx=0,1,4,2,3 eval_params.model=linear,nn log_to_file=False log_level=SUCCESS experiment_name=fed_imp_pc2
+
+# missforest
+python run_evaluation.py --multirun hydra.launcher.n_jobs=-1 dataset_name=hhip \
+data_partition_name=niid-t2,niid-t1 missing_scenario_name=mcar,mar-heter,mar-homog,mnar2-heter,mnar2-homog imputer_name=missforest fed_strategy_name=fedtree,central,local round_idx=7,0,5,2,4,9,8,6,3,1 eval_params.model=linear,nn log_to_file=False log_level=SUCCESS experiment_name=fed_imp_pc2
