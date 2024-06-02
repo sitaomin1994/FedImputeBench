03a3bd0b2943a3e3b6a279db269f3f394a45b8d9
sitao <sitaomin@usc.edu>
2024-06-02 19:30:12 -0400

    bug fix downstream prediction

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
 miwae local,fedavg,fedavg_ft,fedprox,central nn -1 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
 gain local,fedavg,fedavg_ft,fedprox,central nn -1 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
 missforest local,fedtree,central nn -1 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-heter \
 simple,em,linear_ice local,fedavg,central nn -1 fed_imp_pc2
