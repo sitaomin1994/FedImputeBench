8463fd1540342c13e3c5e08eeb6abc953c49d10c
sitao <sitaomin@usc.edu>
2024-06-02 14:52:43 -0400

    bug fix: config persist - fine-tune overwritten. new feature: add use default hyper params option

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mnar2-heter miwae local,fedavg nn 10 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mnar2-heter gain local,fedavg nn 10 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mnar2-heter simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mnar2-heter missforest local,fedtree nn -1 fed_imp_pc2
+./scripts/run_eval.sh codrna niid-f1,niid-f2 mar-heter,mnar2-heter miwae local,fedavg nn 10 fed_imp_pc2
+./scripts/run_eval.sh codrna niid-f1,niid-f2 mar-heter,mnar2-heter gain local,fedavg nn 10 fed_imp_pc2
+./scripts/run_eval.sh codrna niid-f1,niid-f2 mar-heter,mnar2-heter simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
+./scripts/run_eval.sh codrna niid-f1,niid-f2 mar-heter,mnar2-heter missforest local,fedtree nn -1 fed_imp_pc2
 #./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mnar2-heter simple,em,linear_ice,miwae,gain local,fedavg nn -1 fed_imp_pc2
 #./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mnar2-heter missforest local,fedtree nn -1 fed_imp_pc2
--- scripts/cpu2.sh
+++ scripts/cpu2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # dvisits
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter missforest \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-homog missforest \
 local,fedtree linear -1 fed_imp_pc2
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter simple,em,linear_ice \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-homog simple,em,linear_ice \
 local,fedavg linear -1 fed_imp_pc2
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter gain \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-homog gain \
 local,fedavg linear -1 fed_imp_pc2
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter miwae \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-homog miwae \
 local,fedavg linear -1 fed_imp_pc2
 
 # school pca
-./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter missforest \
+./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mar-homog missforest \
 local,fedtree linear -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter simple,em,linear_ice \
+./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mar-homog simple,em,linear_ice \
 local,fedavg linear -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter gain \
+./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mar-homog gain \
 local,fedavg linear -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar_homog,mar-heter,mnar2-homog,mnar2-heter miwae \
+./scripts/run_eval.sh school_pca iid-even,iid-uneven,niid-t1,niid-t2 mar-homog miwae \
 local,fedavg linear -1 fed_imp_pc2
