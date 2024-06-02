852f296fefa1bda8a0b29ad471df559d2f8b625c
sitao <sitaomin@usc.edu>
2024-06-02 19:26:55 -0400

    bug fix downstream prediction

--- scripts/job_scripts_cpu2.sh
+++ scripts/job_scripts_cpu2.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=main
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=60
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=6G
 #SBATCH --time=3-00:00:00
 #SBATCH --export=ALL
 
