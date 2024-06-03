a0d5dc46c3856a8dfd85a73611cc49b5a48520da
sitao <sitaomin@usc.edu>
2024-06-03 08:17:58 -0400

    bug fix downstream prediction

--- scripts/gpu1.sh
+++ scripts/gpu1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-homog \
-miwae local,fedavg,fedavg_ft,fedprox nn 8 fed_imp_pc2
+./scripts/run_eval.sh school_pca niid-f1 mnar2-homog \
+miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+#./scripts/run_eval.sh school_pca niid-f1 mnar2-homog \
+#gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+#./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
+#missforest local,fedtree nn -1 fed_imp_pc2
+#./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
+#simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
 
 
 
