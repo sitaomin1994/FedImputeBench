80ee2b41954ef828f1b19cf5cdb25f8c75cd5d06
sitao <sitaomin@usc.edu>
2024-06-02 20:24:47 -0400

    bug fix downstream prediction

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mnar2-heter \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mcar,mar-homog,mnar2-homog \
 miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mnar2-heter \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mcar,mar-homog,mnar2-homog \
 gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mnar2-heter \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mcar,mar-homog,mnar-homog \
 missforest local,fedtree nn -1 fed_imp_pc2
-./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mnar2-heter \
+./scripts/run_eval.sh dvisits iid-even,iid-uneven,niid-t1,niid-t2 mar-heter,mcar,mar-homog,mnar-homog \
 simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
