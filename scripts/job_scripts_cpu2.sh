6e3687ffe9bd1e6b7fe411da2fe9dac7b53c0eb8
sitao <sitaomin@usc.edu>
2024-06-03 15:39:44 -0400

    federated downstream prediction

--- scripts/job_scripts_cpu2.sh
+++ scripts/job_scripts_cpu2.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=main
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
-#SBATCH --mem-per-cpu=6G
+#SBATCH --cpus-per-task=50
+#SBATCH --mem-per-cpu=4G
 #SBATCH --time=3-00:00:00
 #SBATCH --export=ALL
 
