8438645a933c4a3d27bd65a440ee71e8591ef62a
sitao <sitaomin@usc.edu>
2024-06-03 07:31:51 -0400

    bug fix downstream prediction

--- scripts/gpu1.sh
+++ scripts/gpu1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
-miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-homog \
+miwae local,fedavg,fedavg_ft,fedprox nn 8 fed_imp_pc2
 
 
 
--- scripts/pc1.sh
+++ scripts/pc1.sh
@@ -4,7 +4,7 @@
 #missforest local,fedtree nn -1 fed_imp_pc2
 #./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
 #simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
-./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-homog \
 miwae local,fedavg,fedavg_ft,fedprox nn 8 fed_imp_pc2
-./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-homog \
 gain local,fedavg,fedavg_ft,fedprox nn 10 fed_imp_pc2
