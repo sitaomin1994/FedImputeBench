b9d4ce58db3139431d9d73a52376ab961b065348
sitao <sitaomin@usc.edu>
2024-06-03 04:55:53 -0400

    bug fix downstream prediction

--- scripts/pc2.sh
+++ scripts/pc2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mcar \
-simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
+./scripts/run_eval.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mnar2-homog \
+em,linear_ice local,fedavg nn -1 fed_imp_pc2
