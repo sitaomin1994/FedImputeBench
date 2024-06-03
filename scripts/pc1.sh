9b4f5c2daa9348640609ce3a6bfc144b17e083ed
sitao <sitaomin@usc.edu>
2024-06-02 23:11:00 -0400

    bug fix downstream prediction

--- scripts/pc1.sh
+++ scripts/pc1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mcar \
+./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
+missforest local,fedtree nn -1 fed_imp_pc2
+/scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
+simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
+./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
 miwae local,fedavg,fedavg_ft,fedprox nn 8 fed_imp_pc2
 ./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
 gain local,fedavg,fedavg_ft,fedprox nn 10 fed_imp_pc2
-./scripts/run_eval.sh codrna iid-even,iid-uneven,niid-f1,niid-f2 mar-homog \
-missforest local,fedtree nn -1 fed_imp_pc2
