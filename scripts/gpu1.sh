6da5848a67bf6af2e7fd80de0d125e3149b96755
sitao <sitaomin@usc.edu>
2024-06-03 06:41:06 -0400

    bug fix downstream prediction

--- scripts/gpu1.sh
+++ scripts/gpu1.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-./scripts/run_eval.sh school_pca niid-f2 mar-heter \
+./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
 miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca niid-f2 mar-heter \
-gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca niid-f2 mar-heter \
-missforest local,fedtree nn -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca niid-f2 mar-heter \
-simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
+
 
 
--- scripts/gpu3.sh
+++ scripts/gpu3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./scripts/run_eval.sh school_pca niid-f2 mcar \
-miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca niid-f2 mcar \
-gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca niid-f2 mcar \
+#./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
+#miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+#./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
+#gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
 missforest local,fedtree nn -1 fed_imp_pc2
-./scripts/run_eval.sh school_pca niid-f2 mcar \
-simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
+./scripts/run_eval.sh school_pca niid-f2 mnar2-homog \
+simple local,fedavg nn -1 fed_imp_pc2
