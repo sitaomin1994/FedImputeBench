6956d6558fef6cb51f19d82c66ae32c4e002630e
sitao <sitaomin@usc.edu>
2024-06-03 21:39:26 -0400

    federated downstream prediction

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./scripts/run_eval_fed.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+./scripts/run_eval_fed.sh hhip iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval_fed.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+./scripts/run_eval_fed.sh hhip iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-./scripts/run_eval_fed.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+./scripts/run_eval_fed.sh hhip iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 missforest local,fedtree nn -1 fed_imp_pc2
-./scripts/run_eval_fed.sh vehicle iid-even,iid-uneven,niid-f1,niid-f2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+./scripts/run_eval_fed.sh hhip iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
