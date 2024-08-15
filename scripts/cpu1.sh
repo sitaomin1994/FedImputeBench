2be33e802dddb01e5f1ba352dc7c57ce8ed22be8
sitao <sitaomin@usc.edu>
2024-08-15 07:50:27 -0400

    cpu1

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -4,7 +4,7 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 
 # imputation
 #./scripts/run_scenario_cpu.sh vehicle_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter simple,em,linear_ice local,fedavg -1
-./scripts/run_scenario_cpu.sh vehicle_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
+#./scripts/run_scenario_cpu.sh vehicle_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter simple,em,linear_ice local,fedavg -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
 #./scripts/run_scenario_cpu.sh vehicle_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
@@ -15,8 +15,8 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 # evaluation
 #./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
-#./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
-#missforest local,fedtree nn -1 fed_imp_pc2
+./scripts/run_eval.sh vehicle_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+missforest local,fedtree nn -1 fed_imp_pc2
 #./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
 #./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
