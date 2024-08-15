e8d0bbd79c265e108cb83fd81d36ac45a92b5645
sitao <sitaomin@usc.edu>
2024-08-15 07:50:59 -0400

    cpu1

--- scripts/job_scripts_cpu1.sh
+++ scripts/job_scripts_cpu1.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=p_jsvaidya_1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=15
-#SBATCH --mem-per-cpu=16G
+#SBATCH --cpus-per-task=60
+#SBATCH --mem-per-cpu=4G
 #SBATCH --time=3-00:00:00
 #SBATCH --export=ALL
 
