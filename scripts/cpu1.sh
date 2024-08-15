84b1ad4849b07fe1b767b98ffa40802da1aecd93
sitao <sitaomin@usc.edu>
2024-08-15 08:38:15 -0400

    cpu1

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -15,8 +15,7 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 # evaluation
 #./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
-./scripts/run_eval.sh vehicle_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
-missforest local,fedtree nn -1 fed_imp_pc2
+./scripts/run_eval.sh vehicle_np natural-partition mnar2-heter missforest fedtree nn -1 fed_imp_pc2
 #./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
 #./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
