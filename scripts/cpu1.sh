060be82d359e96f838dbe1ce94d8c6fe98661e24
sitaomin1994 <sitaomin@usc.com>
2024-08-16 17:00:40 -0400

    cpu1

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -7,7 +7,7 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 #./scripts/run_scenario_cpu.sh hhip_sp natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter simple,em,linear_ice local,fedavg -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
-./scripts/run_scenario_cpu.sh hhip_sp natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
+./scripts/run_scenario_cpu.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
 #./scripts/run_scenario_cpu.sh hhip_sp natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter miwae local,fedavg,fedavg_ft,fedprox -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter miwae local,fedavg,fedavg_ft,fedprox -1
@@ -17,7 +17,7 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 #simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
 #./scripts/run_eval.sh hhip_sp natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #missforest local,fedtree nn -1 fed_imp_pc2
-./scripts/run_eval.sh hhip_sp natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+./scripts/run_eval.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
 #./scripts/run_eval.sh hhip_sp natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
