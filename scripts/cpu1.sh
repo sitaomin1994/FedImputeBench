eafcf134bbc49a4a1e81f782814f4deacfbb86fa
sitao <sitaomin@usc.edu>
2024-08-15 23:07:42 -0400

    cpu1

--- scripts/cpu1.sh
+++ scripts/cpu1.sh
@@ -7,19 +7,21 @@ export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 #./scripts/run_scenario_cpu.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter simple,em,linear_ice local,fedavg -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter missforest local,fedtree -1
-./scripts/run_scenario_cpu.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
+#./scripts/run_scenario_cpu.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter gain local,fedavg,fedavg_ft,fedprox -1
-./scripts/run_scenario_cpu.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter miwae local,fedavg,fedavg_ft,fedprox -1
+#./scripts/run_scenario_cpu.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter miwae local,fedavg,fedavg_ft,fedprox -1
 #./scripts/run_scenario_cpu.sh school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter miwae local,fedavg,fedavg_ft,fedprox -1
 
 # evaluation
-#./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
-#simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
-# vehicle_np natural-partition mnar2-heter missforest fedtree nn -1 fed_imp_pc2
-#./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
-#gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
-#./scripts/run_eval.sh vehicle_np,school_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
-#miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+./scripts/run_eval.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+simple,em,linear_ice local,fedavg nn -1 fed_imp_pc2
+./scripts/run_eval.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+missforest local,fedtree nn -1 fed_imp_pc2
+#vehicle_np natural-partition mnar2-heter missforest fedtree nn -1 fed_imp_pc2
+./scripts/run_eval.sh hhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+gain local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
+./scripts/run_eval.sh vhhip_np natural-partition mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
+miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
 #./scripts/run_eval_fed.sh hhip iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
 #miwae local,fedavg,fedavg_ft,fedprox nn -1 fed_imp_pc2
 #./scripts/run_eval_fed.sh hhip iid-even,iid-uneven,niid-t1,niid-t2 mcar,mar-homog,mar-heter,mnar2-homog,mnar2-heter \
